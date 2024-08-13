38bed6f1d3fdbd74fc063b25870cf8cdb0894832
mdalihasanriyad <you@ahriyad.com@gmail.com>
2024-08-13 23:53:14 +0600

    banner price  added

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,5 +3,13 @@ import notFound from "./notFound.png";
 import bannerImageOne from "./banner/bannerImageOne.png";
 import bannerImageTwo from "./bannerPhone.jpg";
 import bannerImageThree from "./bannerHeadPhone.jpg";
+import bannerImageFour from "./banner/bannerImageFour.png";
 
-export { logo, notFound, bannerImageOne, bannerImageTwo, bannerImageThree };
+export {
+  logo,
+  notFound,
+  bannerImageOne,
+  bannerImageTwo,
+  bannerImageThree,
+  bannerImageFour,
+};
